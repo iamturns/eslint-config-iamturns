28c72f84637b7a899dd94541a95c882f6167383a
Matt Turnbull <matt@iamturns.com>
2018-09-13 21:38:02 +1000

    feat: disable react/destructuring-assignment - too restrictive

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ module.exports = {
 	rules: {
 		// Too restrictive. Better readability going from simple to more complex code as scrolling.
 		"no-use-before-define": "off",
+		// Too restrictive: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/destructuring-assignment.md
+		"react/destructuring-assignment": "off",
 		// No jsx extension: https://github.com/facebook/create-react-app/issues/87#issuecomment-234627904
 		"react/jsx-filename-extension": "off",
 		// Allow Prettier to format code
