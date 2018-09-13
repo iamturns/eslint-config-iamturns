0c07186d4a6338b52858075eb9a86007e4d11a4f
Matt Turnbull <matt@iamturns.com>
2018-09-13 20:47:06 +1000

    feat: extend full airbnb eslint config, not just airbnb-base
    This will include rules for React.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = {
-	extends: ["airbnb-base", "plugin:prettier/recommended"],
+	extends: ["airbnb", "plugin:prettier/recommended"],
 	rules: {
 		// Too restrictive. Better readability going from simple to more complex code as scrolling.
 		"no-use-before-define": "off",
