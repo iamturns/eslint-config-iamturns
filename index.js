18155db770dc110040a544275929c322054edea6
Matt Turnbull <matt@iamturns.com>
2018-09-13 21:00:43 +1000

    feat: no jsx extension check

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ module.exports = {
 	rules: {
 		// Too restrictive. Better readability going from simple to more complex code as scrolling.
 		"no-use-before-define": "off",
+		// No jsx extension: https://github.com/facebook/create-react-app/issues/87#issuecomment-234627904
+		"react/jsx-filename-extension": "off",
 	},
 }
